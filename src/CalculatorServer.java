21657ed4bcfd1e8f1611f512d9ddd867f21b96d8
simar <simarjitsingharora437@gmail.com>
2023-07-28 16:36:09 +0930

    Initial commit for new file CalculatorServer which starts the server

---
+++ src/CalculatorServer.java
@@ -0,0 +1,13 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class CalculatorServer {
+    public static void main(String[] args) {
+        try {
+            Calculator calculator = new CalculatorImplementation();
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.rebind("Calculator", calculator);
+            System.out.println("Server Running...");
+        } catch (Exception ignored) {}
+    }
+}
