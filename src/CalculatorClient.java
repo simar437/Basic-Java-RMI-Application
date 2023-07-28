2cf0315d1846239b1af2674e2bdbc3444b08fd4e
simar <simarjitsingharora437@gmail.com>
2023-07-28 16:59:34 +0930

    Testing

--- src/CalculatorClient.java
+++ src/CalculatorClient.java
@@ -1,13 +1,29 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class CalculatorClient {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         try {
             Registry registry = LocateRegistry.getRegistry("localhost");
             Calculator calculator = (Calculator) registry.lookup("Calculator");
-            calculator.pushValue(10);
-            calculator.printStack();
+            int num;
+             do {
+                 System.out.println("Enter your Choice: ");
+                 System.out.println("1 - Push Value");
+                 System.out.println("2 - Print Stack");
+                 System.out.println("-1 - Exit");
+                 num = sc.nextInt();
+                 if (num == 1) {
+                     System.out.print("Enter a number to push in the stack: ");
+                     calculator.pushValue(sc.nextInt());
+                 }
+                 if (num == 2) {
+                     System.out.print("Stack: ");
+                     calculator.printStack();
+                 }
+            } while (num != -1);
 
         } catch (Exception ignore) {}
     }
