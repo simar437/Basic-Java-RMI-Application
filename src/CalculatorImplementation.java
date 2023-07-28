eae2e5b66269bebb1e885da4a4a869d2188eb030
simar <simarjitsingharora437@gmail.com>
2023-07-28 16:26:06 +0930

    Created a stack and implemented pushValue()

--- src/CalculatorImplementation.java
+++ src/CalculatorImplementation.java
@@ -1,13 +1,15 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Stack;
 
 public class CalculatorImplementation extends UnicastRemoteObject implements Calculator {
+    Stack<Integer> mainStack = new Stack<>();
     protected CalculatorImplementation() throws RemoteException {
         super();
     }
 
     @Override
     public void pushValue(int val) throws RemoteException {
-
+        mainStack.push(val);
     }
 }
